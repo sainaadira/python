02cda4502fb1c27798b340051fcf6bc8e4e0f0c0
Sai <snaadira@gmail.com>
2021-10-27 10:41:55 -0400

    added password generator project from day 5 and adding day 6 folder

---
+++ app-brewery/05-beginner/password-generator/main.py
@@ -0,0 +1,64 @@
+
+'''
+###############################################################
+# Password Generator Project
+generate random password based on number of letters/symbols & nums
+user selects in the input
+
+###############################################################
+'''
+
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+password = ''
+# creating a range based on the number of letter user wants
+for chracter in range(1, nr_letters + 1):
+    # looks in list of letters and gives a random letter
+    password += random.choice(letters)
+
+# generating random symbols based on user input
+for symbol in range(1, nr_symbols + 1):
+    password += random.choice(symbols)
+
+# generating random numbers based on user input
+for num in range(1, nr_numbers + 1):
+    password += random.choice(numbers)
+
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+# creating a range based on the number of letter user wants
+for chracter in range(1, nr_letters + 1):
+    # looks in list of letters and gives a random letter
+    password_list.append(random.choice(letters))
+
+# generating random symbols based on user input
+for symbol in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+# generating random numbers based on user input
+for num in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+# randomly shuffling the list
+random.shuffle(password_list)
+
+# turning list into a string
+password = ''
+for char in password_list:
+    password += char
+
+print(f'your password is: {password}')
---
+++ app-brewery/06-beginner/functions/main.py
@@ -0,0 +1,24 @@
+'''
+###############################################################
+# functions
+creating a function
+using the def keyword (def-ining a functing)
+
+syntax:
+
+def my_function():
+  # do something
+  # then do this
+  # funally do this
+  
+my_function()
+###############################################################
+'''
+
+
+def my_function():
+    print('hello')
+    print('bye')
+
+
+my_function()
